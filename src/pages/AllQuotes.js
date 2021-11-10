b6e7b6e56146a335bbc5cefa425a3722e868b943
Cindy Liang <hakkacindy@gmail.com>
2021-11-10 16:07:29 +0000

    add one quote

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -2,7 +2,8 @@ import QuoteList from '../components/quotes/QuoteList';
 
 const DUMMY_QUOTES = [
   { id: 'q1', author: 'Cindy', text: 'learning React is great' },
-  { id: 'q2', author: 'Liang', text: 'This React course is amazing' }
+  { id: 'q2', author: 'Liang', text: 'This React course is amazing' },
+  { id: 'q3', author: 'Lihsin', text: 'This React course is ok' }
 ];
 
 
