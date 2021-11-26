6d40339fcc504541d965b187cab7251c2058cdce
Cindy Liang <hakkacindy@gmail.com>
2021-11-26 23:50:14 +0000

    add useHistory hook to change Url, and useLocation to get the object

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -2,12 +2,25 @@ import { Fragment } from 'react';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const QuoteList = ({
   quotes
 }) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?' + {queryParams})
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {quotes.map((quote) => (
           <QuoteItem
